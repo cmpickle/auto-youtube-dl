cd2c13b0540a8c6593e8263d1a41e2ef0ab1fd0b
Cameron Pickle <cmpickle@gmail.com>
2019-10-14 12:06:10 -0600

    auto sort music into directories

--- rename_music.sh
+++ rename_music.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 rename 's/_/ /g' /media/youtube/music/*
 rename 's/ Official Music Video//gi' /media/youtube/music/*
 rename 's/ Music Video//gi' /media/youtube/music/*
@@ -14,3 +16,12 @@ rename 's/(lyrics)//gi' /media/youtube/music/*
 rename 's/(lyric)//gi' /media/youtube/music/*
 rename 's/(lyrics on screen)//gi' /media/youtube/music/*
 rename 's/()//gi' /media/youtube/music/*
+
+SAVEIFS=$IFS
+IFS=$(echo '\n\b')
+for f in `find /media/youtube/music/ -type f -name "*-*.*"`;
+do
+	mkdir -p /media/youtube/music/$(basename $f | sed -e "s/ -.*//")
+ 	mv $f /media/youtube/music/$(basename $f | sed -e "s/ -.*//")
+done
+IFS=$SAVEIFS
